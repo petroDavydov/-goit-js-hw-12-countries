c5980415617de66f95af8ca43f735a519848d03b
petroDavydov <petrodavydov@gmail.com>
2021-08-21 22:19:16 +0300

    change pnotify on material

--- src/index.js
+++ src/index.js
@@ -5,34 +5,17 @@ import countriesTamplate from "./templates/countriesTamlate.hbs";
 import listTamplate from "./templates/oneCardTemplate.hbs";
 // import { debounce } from "lodash.debounce";
 var debounce = require("lodash.debounce");
-import "@pnotify/core/dist/BrightTheme.css";
-import { error } from "@pnotify/core";
+
+import { error, info, notice } from "@pnotify/core";
+
+import "@pnotify/core/dist/Material.css";
+import "material-design-icons/iconfont/material-icons.css";
+import { defaults } from "@pnotify/core";
+// or
+defaults.styling = "material";
+defaults.icons = "material";
+
 import "./css/style.css";
-
-// function upDateTamplate(data) {
-//   const markUp = countriesTamplate(data);
-
-//   if (data.length > 10) {
-//     error({
-//       text: "Too many matches found. Please enter a more specific query!",
-//     });
-//   } else if (data.status === 404) {
-//     error({
-//       text: "No country has been found. Please enter a more specific query!",
-//     });
-//   } else if (!data.length) {
-//     error({
-//       text: `Please enter a more specific query!`,
-//       styling: "brighttheme",
-//       delay: 500,
-//     });
-//     return;
-//   } else if (data.length === 1) {
-//     refs.ulList.insertAdjacentHTML("afterbegin", markUp);
-//   }
-// }
-
-// // ---
 
 refs.input.addEventListener("input", debounce(searchCountry, 500));
 
@@ -44,7 +27,11 @@ function searchCountry() {
   if (inputValue) {
     fetchCountries(inputValue.trim())
       .then((data) => upDateTamplate(data))
-      .catch((error) => console.log("error"));
+      .catch((error) => {
+        error({
+          text: "Smth gone wrong!",
+        });
+      });
   }
 }
 
@@ -52,32 +39,24 @@ function upDateTamplate(data) {
   const markup = countriesTamplate(data);
   const markupUl = listTamplate(data);
 
-  if (!data.length) {
-    error({
-      text: `Please enter a more specific query!`,
-      styling: "brighttheme",
-      delay: 500,
+  if (!data.length || data.length === "") {
+    info({
+      text: `You enter empty string or Please enter more specific query`,
     });
-    return;
   }
 
-//   if (data && data.length >= 5) {
-//     error({
-//       title: `Too many matches found.`,
-//       text: `We found ${data.length} countries. Please enter a more specific query!`,
-//       styling: "brighttheme",
-//       delay: 500,
-//     });
-//     return (refs.ulList.innerHTML = `<li>${country.name}</li>`);
-//   }
+  if (data.status === 404) {
+    error({
+      text: "No country has been found. Please enter a more specific query!",
+    });
+  }
+
   if (data.length <= 10) {
     refs.ulList.insertAdjacentHTML("beforeend", markupUl);
   }
   if (data.length > 10) {
-    error({
+    notice({
       text: `Please enter a more specific query !`,
-      styling: "brighttheme",
-      delay: 500,
     });
   }
 
